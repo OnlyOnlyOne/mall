9897dcae87e8ce660debc07545f910e7e3f2d296
only <hwy113329@163.com>
2021-12-11 14:55:44 +0800

    MD5加盐

---
+++ src/main/java/com/imooc/mall/common/Constant.java
@@ -0,0 +1,9 @@
+package com.imooc.mall.common;
+
+/**
+ * The type Constant.
+ * 常量值
+ */
+public class Constant {
+    public static final String SALT = "4JF3d5;[.49";
+}
--- src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
+++ src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.imooc.mall.service.impl;
 
+import com.imooc.mall.exception.ImoocMallException;
+import com.imooc.mall.exception.ImoocMallExceptionEnum;
 import com.imooc.mall.model.dao.UserMapper;
 import com.imooc.mall.model.pojo.User;
 import com.imooc.mall.service.UserService;
+import com.imooc.mall.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//@Service
+import java.security.NoSuchAlgorithmException;
+
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
@@ -14,6 +19,29 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser() {
         return userMapper.selectByPrimaryKey(1);
+    }
+
+    @Override
+    public void register(String userName, String password) throws ImoocMallException {
+        //查询用户名是否存在，不允许重名
+        User result = userMapper.selectByName(userName);
+        if (result != null) {
+            //难点：如何巧妙地抛出信息给前面的controller发现
+            throw new ImoocMallException(ImoocMallExceptionEnum.NAME_EXISTED);
+        }
+
+        //写到数据库
+        User user = new User();
+        user.setUsername(userName);
+        try {
+            user.setPassword(MD5Utils.getMD5String(password));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        int count = userMapper.insertSelective(user);
+        if (count == 0) {
+            throw new ImoocMallException(ImoocMallExceptionEnum.INSERT_FAILED);
+        }
     }
 
 
